bf8882cc33b0826c78841cf45536c589da62d988
Lin, Yi-Li <record.nctu.cis91@gmail.com>
2014-09-26 12:27:39 +0800

    ignore if exists

--- pull-linux-settings.sh
+++ pull-linux-settings.sh
@@ -3,6 +3,6 @@
 TEMP_FILE=$(mktemp)
 
 wget -O "$TEMP_FILE" "https://bitbucket.org/recordlyl/linux-settings/get/default.zip"
-unzip -j "$TEMP_FILE" -x "*/.hg_archival.txt" -d "$HOME"
+unzip -jn "$TEMP_FILE" -x "*/.hg_archival.txt" -d "$HOME"
 
 rm "$TEMP_FILE"
